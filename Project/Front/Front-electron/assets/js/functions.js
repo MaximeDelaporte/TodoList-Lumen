c42d11b70821ac1e553d4a436fffa8326be1bd99
qandries <andries.quentin@gmail.com>
2018-03-08 12:27:44 +0100

    add delete task button

--- Project/Front/Front-electron/assets/js/functions.js
+++ Project/Front/Front-electron/assets/js/functions.js
@@ -1,55 +1,44 @@
 $(document).ready(function () {
     let htmlRender = "";
+    let htmlRenderBis = "";
+    let htmlRenderTer = "";
     let i = 1;
+
+    // Todo list Table creation
     $('body').on('click', '[data-use="create-todo-list"]', function () {
         if (document.querySelector('#taskName').value != "" && document.querySelector('#taskDescription').value != "" && document.querySelector('#taskCategory').value != "") {
             let taskName = document.querySelector('#taskName').value;
             let taskDescription = document.querySelector('#taskDescription').value;
             let taskCategory = document.querySelector('#taskCategory').value;
-            localStorage.setItem("1",taskName);
+            /*localStorage.setItem("1",taskName);
             localStorage.setItem("2",taskDescription);
-            localStorage.setItem("3",taskCategory);
+            localStorage.setItem("3",taskCategory);*/
 
             /*$.ajax({
                 type: "POST",
                 url: "",
                 data : {taskName, taskDescription, taskCategory}
             });*/
+            //debugger;
             if (i == 1) {
-                htmlRender += "<table data-todolist='" + i + "'>";
+                htmlRender += "<thead>";
                 htmlRender += "<tr>";
                 htmlRender += "<th>Done</th>";
                 htmlRender += "<th>Task name</th>";
-                htmlRender += "<th>Task Descrition</th>";
+                htmlRender += "<th>Task Description</th>";
                 htmlRender += "<th>Task Category</th>";
                 htmlRender += "</tr>";
-                htmlRender += "<tr>";
-                htmlRender += "<td><input type='checkbox' id='checktodo'></td>";
-                htmlRender += "<td>" + taskName + "</td>";
-                htmlRender += "<td>" + taskDescription + "</td>";
-                htmlRender += "<td>" + taskCategory + "</td>";
-                htmlRender += "<td><button id='delete'>Bin</button></td>";
-                htmlRender += "</tr>";
-                htmlRender += "</table>";
-                i++;
-                $('[data-action="createTable"]').html(htmlRender);
-            } else {
-                htmlRender += "<tr>";
-                htmlRender += "<th> </th>";
-                htmlRender += "<th> </th>";
-                htmlRender += "<th> </th>";
-                htmlRender += "<th> </th>";
-                htmlRender += "</tr>";
-                htmlRender += "<tr >";
-                htmlRender += "<td><input type='checkbox' id='checktodo'></td>";
-                htmlRender += "<td>" + taskName + "</td>";
-                htmlRender += "<td>" + taskDescription + "</td>";
-                htmlRender += "<td>" + taskCategory + "</td>";
-                htmlRender += "<td><button id='delete'>Bin</button></td>";
-                htmlRender += "</tr>";
-                i++;
-                $('[data-todolist="1"]').html(htmlRender);
+                htmlRender += "</thead>";
             }
+            htmlRender += "<tr>";
+            htmlRender += "<td><input type='checkbox' id='checktodo-" + i + "'></td>";
+            htmlRender += "<td>" + taskName + "</td>";
+            htmlRender += "<td>" + taskDescription + "</td>";
+            htmlRender += "<td>" + taskCategory + "</td>";
+            htmlRender += "<td><button name='delete' id='delete-" +  i + "'>Bin</button></td>";
+            htmlRender += "</tr>";
+            i++;
+            $('[data-todolist="1"]').html(htmlRender);
         } if (document.querySelector('#taskName').value == "") {
                     alert("Task Name is required");
         } if (document.querySelector('#taskDescription').value == "") {
@@ -57,18 +46,43 @@ $(document).ready(function () {
         } if (document.querySelector('#taskCategory').value == "") {
                     alert("Task Category is required");
         }
+
+
     });
 
+    // Achieve a task
     $('#mytodolist').on('click', function () {
-        if ($('#checktodo').prop('checked')) {
-            alert('Are you sure to have finish the task? ');
-            $('#checktodo').hide()
-
-        } else {
+        for(let j = 1; j < i; j++){
+            if ($("input[id='checktodo-" + j + "']").prop('checked')) {
+                $('tr:nth-child(' + j + ') td').addClass('taskDone')
+            } else if ($("input[id='checktodo-" + j + "']").prop('checked') == false){
+                $('tr:nth-child(' + j + ') td').removeClass('taskDone')
+            }
         }
     });
-    /*$('#delete').on('click', function () {
 
-    })*/
+    // Delete a task
+    $('#mytodolist').on('click', '[name="delete"]',function () {
+        alert('Are you sure?');
+        let text = document.querySelector('[id^="checktodo-"]');
+        let firstTr = $(text)["0"].id.slice(-1); // take the first checkbox id button for initialize "l" in the loop
 
+        $(this).closest('tr').remove();
+        htmlRenderBis = htmlRender.split('<tr>'); // put the string into an array to make easier the deleting
+        debugger;
+        for (let k = 2, l=firstTr; k < htmlRenderBis.length; k++, l++) {
+            if($(this).closest('tr')["0"].firstChild.children["0"].id.slice(-1) == l){
+                debugger;
+                htmlRenderBis[k] = htmlRenderBis[k].replace(/(<.+)/g, ""); //delete the needed table row
+            }
+        }
+
+        htmlRenderTer = "";
+        $.each(htmlRenderBis, function(index, value) {
+            htmlRenderTer += JSON.stringify(htmlRenderBis[index]); // stringify the Array
+        });
+        htmlRender = htmlRenderTer.replace(/"([^"]+)"/g, "<tr>$1"); // we put back the tr tag
+        htmlRender = htmlRender.replace(htmlRender.slice(0, 4), ""); // we delete the extra tr tag at the start
+        debugger;
+    })
 });
